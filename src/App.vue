a1d878451ef99b014e57e1310ff2e122de41288d
trungnguyenduc0101 <trungnguyenduc0101@gmail.com>
2023-05-25 00:33:55 +0700

    feat: create flip logic

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,59 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { onMounted, ref } from 'vue'
+import Card from './components/Card.vue'
+
+const cardList = ref<{ value: number; visible: boolean; position: number }[]>([])
+
+onMounted(() => {
+  createCardList()
+})
+
+function createCardList() {
+  for (let i = 0; i < 16; i++) {
+    cardList.value.push({
+      value: i,
+      visible: false,
+      position: i
+    })
+  }
+}
+
+function flipCard(payload: {position: number}) {
+  cardList.value[payload.position].visible = !cardList.value[payload.position].visible;
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="game-wrapper">
+    <h1>Peek-a-Vue</h1>
+    <section class="game-board">
+      <Card
+        v-for="(card, index) in cardList"
+        :value="card.value"
+        :visible="card.visible"
+        :key="`card-${index}`"
+        :position="card.position"
+        @select-card="flipCard"
+      />
+    </section>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.game-board {
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  justify-content: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.game-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="card" @click="selectCard">
+        <div v-if="visible" class="card-face is-front">
+            {{ value }}
+        </div>
+        <div v-else class="card-face is-back">
+            Back
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+// import { ref } from 'vue';
+
+const props = defineProps<{
+  value: number,
+  visible: boolean,
+  position: number
+}>()
+
+const emit = defineEmits<{
+  (e: 'select-card', value: {position: number}): void
+}>()
+
+function selectCard() {
+    emit('select-card', {
+        position: props.position
+    })
+}
+
+</script>
+
+<style scoped>
+.card {
+  border: 5px solid #ccc;
+}
+
+.card-face {
+    width: 100%;
+    height: 100%;
+}
+
+.card-face.is-front {
+    background-color: red;
+    color: white
+}
+
+.card-face.is-back {
+    background-color: blue;
+    color: white
+}
+</style>
